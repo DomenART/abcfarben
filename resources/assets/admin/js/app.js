c48930c03f94af306843b865a029870d57916a30
Kundius Ruslan <kundius.ruslan@gmail.com>
2018-11-08 16:46:44 +0300

    upd

--- resources/assets/admin/js/app.js
+++ resources/assets/admin/js/app.js
@@ -23,6 +23,7 @@ const TinyMCEInit = () => {
       plugins: 'preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample code table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help',
       toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat | code',
       image_advtab: true,
+      relative_urls: false,
     })
   }
 }
